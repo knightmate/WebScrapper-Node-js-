672fcd964eecf4bb55eda2399189656ae56f1157
Meghraj Deshmukh <meghrajdeshmukh@Meghrajs-MacBook-Air.local>
2020-05-22 17:54:15 +0530

    heroku

--- config/middleware.js
+++ config/middleware.js
@@ -1,21 +1,7 @@
 module.exports.serverCrash=(req,res,next)=>
 {
     
-    process.on('exit', function(code) {
-
-
-        console.log(`About to exit with code ${code}`);
-         
-        
-       
-          // return res.render('home');
- 
-         return res.send(`<h2>Server Crash</h2>`);
-             
-        
-          
-    
-    });
+   
 
  
     next();
